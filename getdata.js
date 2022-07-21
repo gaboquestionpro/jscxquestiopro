c411a4656501771b422a1e82df6fcf4cf9aa4184
gaboquestionpro <77982601+gaboquestionpro@users.noreply.github.com>
2022-07-20 22:14:41 -0500

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -1,12 +1,6 @@
 function getdata() {  
   
-    //Variables externas desde sitio web
-    var mail = document.getElementById("mail").value; 
-    var usrid = document.getElementById("IDusr").value;
-    var dept = document.getElementById("dep").value;
-    var seg = document.getElementById("seg").value; 
-    
-    alert(mail, usrid, dept, seg);  
+   console.log("funcionó");
     
     }  
    
