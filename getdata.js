81e923c5f8de1f812c68b43b33df156037ff76ba
gaboquestionpro <77982601+gaboquestionpro@users.noreply.github.com>
2022-07-20 22:11:26 -0500

    Add files via upload

---
+++ getdata.js
@@ -0,0 +1,12 @@
+function getdata() {  
+  
+    //Variables externas desde sitio web
+    var mail = document.getElementById("mail").value; 
+    var usrid = document.getElementById("IDusr").value;
+    var dept = document.getElementById("dep").value;
+    var seg = document.getElementById("seg").value; 
+    
+    alert(mail, usrid, dept, seg);  
+    
+    }  
+   
